1ae49145a991c0d68340fb5d72c9fdd46b290492
ShirPanjshir <kylewaynemba5000@gmail.com>
2024-01-30 12:12:05 -0800

    ready to test

--- 551project.py
+++ 551project.py
@@ -1,1 +1,27 @@
-# test test test
+from flask import Flask
+from flask import render_template
+from flask import jsonify
+
+app = Flask(__name__)
+
+crimes = [{"date": 112923, "event": 340790, "case": 2306474, "offense": "assault", "location": "1100 Block of 37th PL"
+           , "disposition": "cleared arrest"},
+          {"date": 113023, "event": 340791, "case": 2306475, "offense": "theft", "location": "1200 Block of 37th PL"
+              , "disposition": "open"},
+          {"date": 113023, "event": 340792, "offense": "larceny", "location": "1100 Block of 37th PL"
+              , "disposition": "open"},
+          {"date": 113123, "event": 340793, "case": 2306476, "offense": "assault", "location": "1500 Block of 37th PL"
+              , "disposition": "cleared arrest"},
+          ]
+
+
+@app.route("/")
+def hello_world():
+    return render_template('home.html', crimes=crimes)
+
+@app.route("/api/crimes")
+def list_crimes():
+    return jsonify(crimes)
+
+if __name__ == "__main__":
+    app.run()
